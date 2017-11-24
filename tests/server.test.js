2bbf952d638c450a9a90ccae2f7dece4ec7c005a
Alon <alontalmor@gmail.com>
2017-11-24 22:18:29 +0200

    added tests directory

---
+++ tests/server.test.js
@@ -0,0 +1,11 @@
+const server = require('../server');
+
+it('should succeed', () => {
+    if(1 !== 1) {
+        throw new Error('it should not happen');
+    }
+});
+
+// it('should fail', () => {
+//     throw new Error('it should fail');
+// });
