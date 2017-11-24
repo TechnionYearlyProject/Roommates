c4bdcb775b856722e45870c9b3e64ba7954779b2
Alon <alontalmor@gmail.com>
2017-11-24 19:08:07 +0200

    no special update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 //app.use(express.static(__dirname + '/public')); //register middleware
 
 app.get('/', (request, response) => {
-    response.send('<h1>מה קורה גינגי?</h1>');
+    response.send('<h1>Roommates..</h1><p>you can send me your credit card number if you want :)</p>');
 });
 
 app.listen(port, () => {
