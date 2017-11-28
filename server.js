fdcde306dadfc6662cae89e907538130e5680e85
unknown <abramovich.or@gmail.com>
2017-11-28 19:43:27 +0200

    CI: #50 - add Heroku integration to the CI

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 //app.use(express.static(__dirname + '/public')); //register middleware
 
 app.get('/', (request, response) => {
-    response.send('<h1>Roommates..</h1><p>you can send me your credit card number if you want :)</p>');
+    response.send('<h1>CI Test</h1><h1>Roommates..</h1><p>you can send me your credit card number if you want :)</p>');
 });
 
 app.listen(port, () => {
