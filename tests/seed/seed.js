d41f94147dfb2dde45ca7a010b36267797b037e1
Or <abramovich.or@gmail.com>
2017-12-12 23:06:47 +0200

    Testing: Adding data to the seed

--- tests/seed/seed.js
+++ tests/seed/seed.js
@@ -65,6 +65,55 @@ const user1 = {
     firstName: 'user1_firstName',
     lastName: 'user1_lastName',
     birthdate: '1992-06-24',
+    gender: 'male',
+    hobbies: [1,2,3]
+};
+
+const user2 = {
+    email: 'user2@gmail.com',
+    password: '123456',
+    firstName: 'user2_firstName',
+    lastName: 'user2_lastName',
+    birthdate: '1992-06-24',
+    gender: 'male',
+    hobbies: [4,5,6]
+};
+
+const user3 = {
+    email: 'user3@gmail.com',
+    password: '123456',
+    firstName: 'user3_firstName',
+    lastName: 'user3_lastName',
+    birthdate: '1992-06-24',
+    gender: 'male',
+    hobbies: [7,8,9]
+};
+
+const user4 = {
+    email: 'user4@gmail.com',
+    password: '123456',
+    firstName: 'user4_firstName',
+    lastName: 'user4_lastName',
+    birthdate: '1992-06-24',
+    gender: 'male',
+    hobbies: [4,5,7]
+};
+
+const user5 = {
+    email: 'user5@gmail.com',
+    password: '123456',
+    firstName: 'user5_firstName',
+    lastName: 'user5_lastName',
+    birthdate: '1992-06-24',
+    gender: 'female'
+};
+
+const user6 = {
+    email: 'user6@gmail.com',
+    password: '123456',
+    firstName: 'user6_firstName',
+    lastName: 'user6_lastName',
+    birthdate: '1992-06-24',
     gender: 'male'
 };
 
@@ -74,7 +123,12 @@ const apartments = [
 ];
 
 const users = [
-    user1
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6
 ];
 
 const coords = {
@@ -82,29 +136,47 @@ const coords = {
     technionIsrael: [35.020568, 32.776515]
 };
 
+var populatedUsers = [];
+
+const populateUsers = (done) => {
+    User.remove({})
+        .then(() => {
+            populatedUsers[0] =  new User(users[0]);
+            populatedUsers[1] =  new User(users[1]);
+            populatedUsers[2] =  new User(users[2]);
+            populatedUsers[3] =  new User(users[3]);
+            populatedUsers[4] =  new User(users[4]);
+            populatedUsers[5] =  new User(users[5]);
+            return Promise.all([populatedUsers[0].save(), 
+                                populatedUsers[1].save(),
+                                populatedUsers[2].save(),
+                                populatedUsers[3].save(),
+                                populatedUsers[4].save(),
+                                populatedUsers[5].save()
+            ]);
+        })
+        .then(() => done())
+        .catch(done);
+};
 
 const populateApartments = (done) => {
+    apartment1._interested.push( populatedUsers[0]._id);
+    apartment1._interested.push( populatedUsers[1]._id);
+    apartment1._interested.push( populatedUsers[2]._id);
     Apartment.remove({})
         .then(() => Apartment.insertMany(apartments))
         .then(() => done())
         .catch(done);
 };
 
-const populateUsers = (done) => {
-    User.remove({})
-        .then(() => {
-            const user1 = new User(users[0]).save();
-    
-            return Promise.all([user1]);
-        })
-        .then(() => done())
-        .catch(done);
-};
+
+
 
 module.exports = {
     apartments,
     users,
     coords,
     populateApartments,
-    populateUsers
+    populateUsers,
+    populatedUsers
 };
