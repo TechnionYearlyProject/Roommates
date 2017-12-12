810246cf32cede8adbfc1ce09adc9ba530c58c68
Or <abramovich.or@gmail.com>
2017-12-12 23:01:14 +0200

    Back-end: Added matcher module (#60)

---
+++ server/logic/matcher.js
@@ -0,0 +1,11 @@
+const {getHobbiesScore} = require('../models/hobbie');
+const array_functions = require('../helpers/array_functions');
+
+const getMatchScore = (hobbiesSetA, hobbiesSetB) => {
+	var matchedHobbies = array_functions.findMatchingValuesInArrays(hobbiesSetA, hobbiesSetB);
+    return getHobbiesScore(matchedHobbies);
+};
+
+module.exports = {
+    getMatchScore
+};
