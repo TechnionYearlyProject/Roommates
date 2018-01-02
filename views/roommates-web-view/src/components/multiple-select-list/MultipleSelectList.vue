f920915eb03225719f7669d1d38a4f76d3d6c185
Or <abramovich.or@gmail.com>
2018-01-02 15:10:15 +0200

    Front-end: Created generic multiple select list (#129)

---
+++ views/roommates-web-view/src/components/multiple-select-list/MultipleSelectList.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="panel panel-default">
+      <div class="btn-group btn-group-toggle" data-toggle="buttons">
+        <label class="btn btn-secondary" v-for="item in items" v-bind:class="{ active: selected_items.indexOf(item._id) >= 0}">
+          <input type="checkbox" autocomplete="off" :id=item._id> {{item.text}}
+        </label>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+         props: [
+          'items',
+          'selected_items'
+        ]
+    }
+</script>
+
+<style scoped>
+.btn-secondary:not([disabled]):not(.disabled).active, .btn-secondary:not([disabled]):not(.disabled):active, .show>.btn-secondary.dropdown-toggle{
+  background-color: orange;
+  border-color: orange;
+}
+</style>
