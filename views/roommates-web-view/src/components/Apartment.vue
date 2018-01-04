7d0d43caf5f762377cf35ea3e1bc092411642ec2
Chanan Ben Tal <chananbental@gmail.com>
2018-01-04 22:31:01 +0200

    small change

--- views/roommates-web-view/src/components/Apartment.vue
+++ views/roommates-web-view/src/components/Apartment.vue
@@ -17,7 +17,6 @@
                         @sliding-start="onSlideStart"
                         @sliding-end="onSlideEnd"
                         v-model="apartment.images">
-
                     </b-carousel>
 
                   </div>
