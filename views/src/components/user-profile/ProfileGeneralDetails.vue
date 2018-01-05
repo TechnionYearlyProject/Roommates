e7f40aa82594053d2e30bd536a0bc51754db2a3a
Or <abramovich.or@gmail.com>
2018-01-06 01:26:57 +0200

    Frontend: added profile general details component - part of #135

---
+++ views/src/components/user-profile/ProfileGeneralDetails.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="card-text">
+      <icon name="mobile"/> {{user_details.mobilephone}} | <icon name="envelope"/> {{user_details.email}} | <icon name="birthday-cake"/> {{user_details.birthdate}} |
+      <span v-if="user_details.gender === 'male'" ><icon name="male"/> Male</span>
+      <span v-if="user_details.gender === 'female'" ><icon name="female"/> Female</span>
+</div>
+</template>
+
+<script>
+    import Icon from 'vue-awesome/components/Icon';
+    export default {
+         props: [
+          'user_details'
+        ],
+        components:{
+          Icon
+        }
+    }
+</script>
+
+<style scoped>
+.card-text {
+    background-color: #fdc600;
+    border-color: #fdc600;
+    text-transform: uppercase;
+    line-height: 1.5;
+    color: #fff;
+    display: inline-block;
+    text-align: left;
+    white-space: nowrap;
+}
+</style>
