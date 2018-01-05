271f985ef4481b9d7adeffc287296c72367fbace
Or <abramovich.or@gmail.com>
2018-01-06 01:27:22 +0200

    Frontend: added profile picture component - part of #135

---
+++ views/src/components/user-profile/ProfilePicture.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+      <b-img center rounded="circle" width="150" height="150" :src=img_src alt="img" class="m-1" />  
+</div>
+</template>
+
+<script>
+    import bImg from 'bootstrap-vue/es/components/image/img';
+    export default {
+         props: [
+          'img_src'
+        ],
+        components:{
+          bImg
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
