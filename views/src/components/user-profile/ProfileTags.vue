86ad85edebb47f17f19c66ca0716b45acdea571a
Idan Yadgar <idanyadgar@gmail.com>
2018-01-07 18:46:44 +0200

    Setting key for element with v-for (in order to avoid compilation warnings)

--- views/src/components/user-profile/ProfileTags.vue
+++ views/src/components/user-profile/ProfileTags.vue
@@ -1,29 +1,32 @@
 <template>
-  <b-button-group>
-    <b-button size="lg" variant="primary" v-for="tag in tags" >{{tag.text}}</b-button>
-  </b-button-group>
+    <b-button-group>
+        <b-button size="lg" variant="primary"
+                  v-for="tag in tags" :key="tag">{{ tag.text }}
+        </b-button>
+    </b-button-group>
 </template>
 
 <script>
-    import bButton from 'bootstrap-vue/es/components/button/button';
+    import bButton from 'bootstrap-vue/es/components/button/button'
+    import bButtonGroup from 'bootstrap-vue/es/components/button-group/button-group'
 
     export default {
-         props: [
-          'tags'
+        props: [
+            'tags'
         ],
-        components:{
-          bButton
+        components: {
+            bButton, bButtonGroup
         }
     }
 </script>
 
 <style scoped>
 
-.btn-primary, .btn-primary:hover, .btn-primary:active, .btn-primary.active {
-    color: #fff !important;
-    background-color: #fdc600 !important;
-    border-color: #fdc600 !important;
-    margin: 5px;
-}
+    .btn-primary, .btn-primary:hover, .btn-primary:active, .btn-primary.active {
+        color: #fff !important;
+        background-color: #fdc600 !important;
+        border-color: #fdc600 !important;
+        margin: 5px;
+    }
 
 </style>
