5e6e33ca92a0d27e19c62964b2230243df13a910
Or <abramovich.or@gmail.com>
2018-01-18 18:39:56 +0200

    Back-end: added logger & logger config #59

---
+++ server/services/logger/logger-config.js
@@ -0,0 +1,24 @@
+
+const fs = require('fs');
+const path = require('path');
+
+exports.getLoggerToFileConfig = function () {
+	const logDirectory = path.join(__dirname, 'server_logs');
+	const logFileName = path.join(logDirectory, 'server.log');
+	fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+	const tsFormat = () => (new Date()).toLocaleTimeString();
+
+    return  {
+	    		"filename": logFileName,
+	      		"timestamp": tsFormat,
+	      		"datePattern": 'yyyy-MM-dd',
+	      		"colorize": 'true',
+	      		"maxsize": 10000000,
+			    "maxFiles": 10,
+	      		"prepend": true,
+	      		"level":  process.env.ENV === 'development' ? 'debug' : 'info',
+	      		"handleExceptions": true,
+	            "humanReadableUnhandledException": true
+        	};
+};
---
+++ server/services/logger/logger.js
@@ -0,0 +1,36 @@
+
+const winston = require('winston');
+require('winston-daily-rotate-file');
+
+const {getLoggerToFileConfig} = require('./logger-config');
+
+var logger = new (winston.Logger)({
+	transports: [
+	 	new (winston.transports.Console)(),
+		new (winston.transports.DailyRotateFile)(getLoggerToFileConfig())
+	]
+});
+
+var logError = (msg) => {
+	logger.error(msg);
+};
+
+var logWarning = (msg) => {
+	logger.warn(msg);
+};
+
+var logInfo = (msg) => {
+	logger.info(msg);
+};
+
+var logDebug = (msg) => {
+	logger.debug(msg);
+};
+
+module.exports = {
+	logger,
+	logError,
+	logWarning,
+	logInfo,
+	logDebug
+};
