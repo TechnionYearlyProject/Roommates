b3b0f78aea088b18eb252fbd125701a297af8148
Alon <alontalmor@gmail.com>
2018-01-19 23:03:05 +0200

    removed /dist/ to deploy to heroku

---
+++ views/dist/static/js/tags.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    key: 'AC',
+    name: 'Air Conditioner',
+    vicon: 'snowflake-o'
+  },
+  {
+    key: 'PETS',
+    name: 'Pets',
+    vicon: 'paw'
+  },
+  {
+    key: 'ELEVATOR',
+    name: 'Elevator',
+    vicon: 'elevator'
+  },
+  {
+    key: 'TERRACE',
+    name: 'Balcony',
+      vicon: 'balcony'
+  },
+  {
+    key: 'PARK',
+    name: 'Parking',
+    vicon: 'car'
+  },
+  {
+    key: 'STORAGE',
+    name: 'Storage',
+    vicon: 'archive'
+  },
+  {
+    key: 'KOSHER',
+    name: 'Kosher Kitchen',
+      vicon: 'cutlery'
+  },
+  {
+    key: 'FURNISHED',
+    name: 'Furnished',
+    vicon: 'truck'
+  }
+];
