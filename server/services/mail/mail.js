36f78483548490075ed5c6fddf9b125136acce0d
Alon <alontalmor@gmail.com>
2018-03-29 19:48:06 +0300

    Services: Added documentation to mail and user-verificator services. issues: #209 #210 #211 #212 #213

--- server/services/mail/mail.js
+++ server/services/mail/mail.js
@@ -1,3 +1,15 @@
+/**
+ * @author: Alon Talmor
+ * @date: 29/3/18
+ * A service that provides mail services.
+ * First we need to set up the OAuth2 key in order to authenticate
+ * with the SMTP service.
+ * After setting up the key successfully, it is possible to start
+ * sending mails using the sendMail() procedure.
+ * -
+ * If you dont have the mail.json file (which is defining the key),
+ * contact me and I'll sent it to you.
+ */
 const sgMail = require('@sendgrid/mail');
 const { logInfo, logError } = require('../logger/logger');
 
@@ -14,6 +26,18 @@ try {
   logError('No "mail.json" exists! Ask Alon for the file.');
 }
 
+/**
+ * @author: Alon Talmor
+ * @date: 29/3/18
+ * This function send an email.
+ * @param: msg - the message to send.
+ * msg is an object with the following structure:
+ * @prop: to: Email address to send the mail to. Example: you@example.com
+ * @prop: from: Email address to send the mail from. Example: me@example.com
+ * @prop: subject: Email message title.
+ * @prop: text: Email message text.
+ * @prop: html: It is possible to send formatted message using html syntax.
+ */
 const sendMail = (msg) => {
   sgMail.send(msg);
 };
--- server/services/user-verification/user-verificator.js
+++ server/services/user-verification/user-verificator.js
@@ -1,3 +1,16 @@
+/**
+ * @author: Alon Talmor
+ * @date: 29/3/18
+ * Use this service to send user verification codes by mail
+ * and to verify verification requests.
+ * User verification codes are used to verify that a new user has filled up
+ * a valid email address during registeration.
+ * -
+ * There should be 2 places where a verification email should be sent:
+ * 1. The first is right after a successful user registeration.
+ * 2. The second one is when an unverified user is requesting to resend verification email
+ *    (might be necessary if the user has't recieved the first verification email during registration).
+ */
 const jwt = require('jsonwebtoken');
 
 const mail = require('../mail/mail');
@@ -6,7 +19,17 @@ const { User } = require('../../models/user');
 
 const supportMail = 'support@roommatesyearlyproject.com';
 const EMAIL_SECRET = 'change_this_to_emailSecret'; //need to set EMAIL_SECRET env variable (also in real server)
-
+const verificationEmailExperationTime = '1d';
+/**
+ * @author: Alon Talmor
+ * @date: 29/3/18
+ * The main function for sending verification email.
+ * First the function creates a jwt token, which encrypts the requesting user id.
+ * This token should be expired after a set amout of time.
+ * After creating the token, an email is sent to the user using the email address
+ * the user has used during registration. The body of the mail includes a url to
+ * the appropriate verification page. The server is responsible to handle such requests.
+ */
 const sendVerificationEmail = (user) => {
   jwt.sign(
     {
@@ -14,7 +37,7 @@ const sendVerificationEmail = (user) => {
     },
     EMAIL_SECRET,
     {
-      expiresIn: '1d'
+      expiresIn: verificationEmailExperationTime
     },
     (err, EmailToken) => {
       if (err) {
@@ -39,6 +62,16 @@ const sendVerificationEmail = (user) => {
   );
 };
 
+/**
+ * @author: Alon Talmor
+ * @date: 29/3/18
+ * Verify a jwt verification token. This token should have been created by sendVerificationEmail().
+ * The first step is to derive the user id from the token. This id can be used next to find the user
+ * in the database. We expect the id to be valid, if it's not we expect the DB search method to reject
+ * the request (this means the Promise.reject() will be returned).
+ * Otherwise, the user's "isVerfied" property is updated to true,
+ * and the verification process has completed successfully.
+ */
 const verifyUser = (token) => {
   const { id } = jwt.verify(token, EMAIL_SECRET);
   return User.findByIdAndUpdate(id, { $set: { isVerified: true } });
