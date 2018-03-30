d7eb4c50c8ae8f4f5d9e56e4c068b8720badebf7
Or <abramovich.or@gmail.com>
2018-03-30 09:16:25 +0300

    Back-end: Added support default entry to the server config file
    The server now handles "default" key with entries of default settings. Settings of the specific environment should be prioritize higher.
    
    #217

--- server/server-config.js
+++ server/server-config.js
@@ -4,12 +4,24 @@ const serverConfig = require('./server-config.json');
 
 const env = process.env.NODE_ENV || 'development';
 
-const setEnvironmentVariables = () => {
-  const envConfig = serverConfig[env];
+
+const setEnvironmentVariablesByEnv = (enviro) => {
+  const envConfig = serverConfig[enviro];
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
   });
 };
+
+const setEnvironmentVariables = () => {
+  setEnvironmentVariablesByEnv(env);
+};
+
+const setDefaultEnvironmentVariables = () => {
+	setEnvironmentVariablesByEnv('default');
+};
+
+
+setDefaultEnvironmentVariables();
 
 if (env === 'development' || env === 'test') {
   setEnvironmentVariables();
@@ -18,4 +30,5 @@ if (env === 'development' || env === 'test') {
 console.log(`Enviroment: 			${colors.green(env)}`);
 console.log(`Port:       			${colors.green(process.env.PORT)}`);
 console.log(`Database:   			${colors.green(process.env.MONGODB_URI)}`);
+console.log(`Log Level:   			${colors.green(process.env.LOG_LEVEL)}`);
 console.log('');
