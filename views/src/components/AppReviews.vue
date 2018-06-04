25d9e6d909a1fcb12297c6b3aa1cd9334573a6b1
Or <abramovich.or@gmail.com>
2018-06-05 00:41:44 +0300

    Front-end: Integrate reviews page with backend Front End: add review to review page Front-end: Provide search functionality in the reviews page
    This commit includes the parent component that gets and sets data of its childs: tile, search and add review. The commit includes the integration of all child components into the parent and integration of the parent with the back-end so all reviews functionalities work now.
    
    #499, #501, #514

--- views/src/components/AppReviews.vue
+++ views/src/components/AppReviews.vue
@@ -1,14 +1,32 @@
+/**
+ * @Modified by: Or Abramovich
+ * @date: 06/18
+ * Convert from static page to dynamic page i.e communicates and fetches data from the back-end
+ */
 <template>
     <div>
         <v-container>
+            <AppReviewAddDialog v-show="dialog" @close="dialog=false;"></AppReviewAddDialog>
+            <AppReviewSearchDialog v-show="search_review" @close="search_review=false;"></AppReviewSearchDialog>
             <div style="position: relative; height: 500px">
                 <gmap-map :center="position" :zoom="15" map-type-id="roadmap" :style="{ width: '100%', height: '500px', position: 'absolute', bottom: 0 }">
                     <GmapMarker :position="position" />
                 </gmap-map>
                 <h1 class="address">
-                    {{ address }}
+                    {{ curReviewAddress }}
                 </h1>
             </div>
+            <v-layout column class="fab-container">
+              <v-btn small fab color="pink accent-1" dark  @click.native.stop="dialog = !dialog" v-if="isVerified">
+              	<v-icon>add</v-icon>
+            	</v-btn>
+              <v-btn small fab color="pink accent-1" dark  @click.native.stop="search_review = !search_review">
+                <v-icon>search</v-icon>
+              </v-btn>
+              <v-btn small fab color="pink accent-1" dark @click="$vuetify.goTo(0, scrollOptions)">
+                <v-icon>keyboard_arrow_up</v-icon>
+              </v-btn>
+            </v-layout>
         </v-container>
 
         <v-container>
@@ -58,103 +76,118 @@
                 </v-card-text>
             </v-card>
         </v-container>
-
         <v-container grid-list-lg>
             <v-layout wrap row>
                 <v-flex xs12 sm6 md6 lg4 v-for="(review ,i) in reviews" :key="`review-${i}`">
-                    <app-review-tile :review="review"></app-review-tile>
+                    <app-review-tile :review="review" :user="getPublisher(review)"></app-review-tile>
                 </v-flex>
             </v-layout>
         </v-container>
+
     </div>
+
 </template>
 
 <script>
   import AppAvatar from './sub-components/AppAvatar';
   import AppReviewTile from './sub-components/AppReviewTile';
+  import AppReviewAddDialog from './sub-components/AppReviewAddDialog';
+  import AppReviewSearchDialog from './sub-components/AppReviewSearchDialog';
+  import StarRating from 'vue-star-rating'
+  import { mapGetters } from 'vuex';
 
   export default {
     name: 'AppReviews',
     data() {
       return {
+       dialog: false,
+       search_review: false,
         position: {
-          lat: 32.7825085,
-          lng: 35.0157237
+          lat: 32.776515,
+          lng: 35.020568
         },
-        address: 'Menahem Begin 40, Holon',
-        reviews: [
-          {
-            createdBy: 'Idan Yadgar',
-            createdAt: new Date(2018, 4, 28).getTime(),
-            ratedCharacteristics: {
-              parking: 1,
-              publicTransport: 2,
-              noise: 3,
-              commercialServices: 4,
-              upkeep: 5,
-              generalRating: 5,
-            },
-            pros: 'Pros Pros Pros Pros Pros Pros',
-            cons: 'Cons Cons Cons Cons Cons Cons '
-          },
-          {
-            createdBy: 'Idan Yadgar',
-            createdAt: new Date(2018, 4, 28).getTime(),
-            ratedCharacteristics: {
-              parking: 1,
-              publicTransport: 2,
-              noise: 3,
-              commercialServices: 4,
-              upkeep: 5,
-              generalRating: 5,
-            },
-            pros: 'Pros Pros Pros Pros Pros Pros',
-            cons: 'Cons Cons Cons Cons Cons Cons '
-          },
-          {
-            createdBy: 'Idan Yadgar',
-            createdAt: new Date(2018, 4, 28).getTime(),
-            ratedCharacteristics: {
-              parking: 1,
-              publicTransport: 2,
-              noise: 3,
-              commercialServices: 4,
-              upkeep: 5,
-              generalRating: 5,
-            },
-            pros: 'Pros Pros Pros Pros Pros Pros',
-            cons: 'Cons Cons Cons Cons Cons Cons '
-          }
-        ]
+        defaultReviewStreet: 'Sderot David Rose',
+        defaultReviewCity: 'haifa',
+        curReviewAddress: '',
+     	  reviews: [],
+     	  locationData: [],
+        usersList: []
       };
     },
+    methods: {
+	    loadReviews(long, lat, city, street){
+	    	 this.$store.dispatch('getReviews', { long: long, lat: lat }).then((reviews) => {
+          const id = reviews.map(review => review._createdBy);
+          reviews.forEach(review => {
+            review.ratedCharacteristics.generalRating = this.getReviewGeneralRating(review);
+          });
+          this.$store.dispatch('fetchUser', { id }).then((users) => {
+            this.usersList = users;
+            this.reviews = reviews;
+            this.curReviewAddress = street.toUpperCase() + ", " + city.toUpperCase();
+          });
+	    	 });
+	    },
+      getReviewGeneralRating(review){
+        return (review.ratedCharacteristics.parking + review.ratedCharacteristics.publicTransport + review.ratedCharacteristics.noise + review.ratedCharacteristics.commercialServices +review.ratedCharacteristics.upkeep)/5;
+      },
+      getPublisher(review) {
+       return this.usersList[review._createdBy];
+      },
+	    setAddress(data) {
+       		this.locationData = data;
+          this.position.lat = data.latitude;
+          this.position.lng = data.longitude; 
+       		this.loadReviews(this.locationData.longitude, this.locationData.latitude, this.locationData.locality, this.locationData.route);
+      },
+    },
+	beforeMount() {
+    if(this.$route.query.lat !== undefined)
+      this.position.lat = parseFloat(this.$route.query.lat);
+    if(this.$route.query.lng !== undefined)
+      this.position.lng = parseFloat(this.$route.query.lng);
+    var street = this.defaultReviewStreet;
+    var city = this.defaultReviewCity;
+    if(this.$route.query.city !== undefined)
+      city = this.$route.query.city;
+    if(this.$route.query.street !== undefined)
+      street = this.$route.query.street;
+		this.loadReviews(this.position.lng, this.position.lat, city, street);
+	},
+	mounted() {
+  },
     computed: {
+      ...mapGetters(['isVerified', 'getUser']),
       totalStatistics() {
+        var reviewsLength = Math.max(this.reviews.length, 1);
         return {
           parking:
                 this.reviews.reduce((t, c) => t + c.ratedCharacteristics.parking, 0) /
-                this.reviews.length,
+                reviewsLength,
           publicTransport:
                 this.reviews.reduce((t, c) => t + c.ratedCharacteristics.publicTransport, 0) /
-                this.reviews.length,
+                reviewsLength,
           noise:
                 this.reviews.reduce((t, c) => t + c.ratedCharacteristics.noise, 0) /
-                this.reviews.length,
+                reviewsLength,
           commercialServices:
                 this.reviews.reduce((t, c) => t + c.ratedCharacteristics.commercialServices, 0) /
-                this.reviews.length,
+                reviewsLength,
           upkeep:
                 this.reviews.reduce((t, c) => t + c.ratedCharacteristics.upkeep, 0) /
-                this.reviews.length,
+                reviewsLength,
           generalRating:
                 this.reviews.reduce((t, c) => t + c.ratedCharacteristics.generalRating, 0) /
-                this.reviews.length
+                reviewsLength
         };
       }
     },
     components: {
       AppReviewTile,
-      AppAvatar
+      AppAvatar,
+      StarRating,
+      AppReviewAddDialog,
+      AppReviewSearchDialog
     }
   };
 </script>
@@ -178,4 +211,11 @@
         white-space: nowrap;
         vertical-align: sub;
     }
+
+  .fab-container {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    z-index:99;
+  }
 </style>
