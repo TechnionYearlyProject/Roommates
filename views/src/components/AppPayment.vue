b2bc7364aa3e90b0e9f7a643ff09d21b4f2361b3
Idan Yadgar <idanyadgar@gmail.com>
2018-06-04 12:54:41 +0300

    Payment page: Added complete event handler.

--- views/src/components/AppPayment.vue
+++ views/src/components/AppPayment.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <v-container>
-            <app-paypal :amount="amount"></app-paypal>
+            <app-paypal :amount="amount" @completed="paymentSuccess"></app-paypal>
         </v-container>
     </div>
 </template>
@@ -21,6 +21,13 @@
       return {
         amount: 100
       };
+    },
+    methods: {
+      paymentSuccess(payment) {
+        if (payment.state === 'approved') {
+          alert(`success!\nID: ${payment.id}`);
+        }
+      }
     },
     components: {
       AppPaypal
