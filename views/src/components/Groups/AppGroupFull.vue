2022a1e2fe31dcd3efb223e9483bcb74fefa5b0d
Alon <alontalmor@gmail.com>
2018-06-08 15:27:35 +0300

    front-end: - myGroup components now communicated with back-end. - lists are now showing names instead of ids. issue: #530 #515 #507

--- views/src/components/Groups/AppGroupFull.vue
+++ views/src/components/Groups/AppGroupFull.vue
@@ -24,7 +24,7 @@
             <v-toolbar-title>Create Your Own Group</v-toolbar-title>
         </v-toolbar>
       <v-container>
-        <app-my-group :members="interestedList" :myGroupMaxSize="requiredRoommates" @submit="submit($event)"></app-my-group>
+        <app-my-group :apartmentId="apartmentId" :interested="interestedList" :myGroupMaxSize="requiredRoommates" @submit="submit($event)"></app-my-group>
       </v-container>
     </v-card>
   </v-container>
@@ -48,7 +48,7 @@ export default {
     },
     interestedList: {
       type: Array,
-      default: () => ["5b140ad395d5b923886ffb43", "5b140fae95d5b923886ffb54", "5b14141895d5b923886ffb5d", "5b14149295d5b923886ffb5f", "5b1414c195d5b923886ffb61"]
+      default: () => null
     }
   },
   data() {
