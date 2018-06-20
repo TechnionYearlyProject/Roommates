e7ce28ee65c29b02e22ff6bc94e6c56c05272986
Alon <alontalmor@gmail.com>
2018-06-20 14:07:57 +0300

    deploy-7

--- views/config/prod.env.js
+++ views/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"https://theroommatesweb.azurewebsites.net"',
-  SOCKET_API: '"https://theroommatesweb.azurewebsites.net/"'
+  ROOT_API: '"https://theroommates.azurewebsites.net"',
+  SOCKET_API: '"https://theroommates.azurewebsites.net/"'
 }
